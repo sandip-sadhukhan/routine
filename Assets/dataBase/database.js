33f4ec2dec43a6ff747672f5ae04e5d9295dda7a
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-01-28 21:39:10 +0530

    8th sem routine update

--- Assets/dataBase/database.js
+++ Assets/dataBase/database.js
@@ -1,208 +1,208 @@
 //Get All Data
 class Day {
-	getSubject(weekday, period) {
-		let result
-		if (weekday == 2) {
-			// TuesDay
-			switch (period) {
-				case 1:
-					result = 'Neural Networks & Deep Learning <br>(VKS)'
-					break
-				case 2:
-					result = 'Project Management & Entrepreneurship<br>(RPC)'
-					break
-				case 3:
-					result = 'Multimedia Technology<br>(AD)'
-					break
-				case 4:
-					result = 'Machine Learning<br>(DS)'
-					break
-				case 5:
-					result = 'Lunch Time'
-					break
-				case 6:
-					result = 'Library'
-					break
-				case 7:
-					result = 'Project 2<br>(TP)'
-					break
-				case 8:
-					result = 'Project 2<br>(TP)'
-					break
-				case 9:
-					result = 'Project 2<br>(TP)'
-					break
-				default:
-					result = 'Something is Wrong'
-			}
-		} else if (weekday == 3) {
-			// Wednesday
-			switch (period) {
-				case 1:
-					result = 'Project Management & Entrepreneurship<br>(RPC)'
-					break
-				case 2:
-					result = 'Neural Networks & Deep Learning <br>(VKS)'
-					break
-				case 3:
-					result = 'Multimedia Technology<br>(AD)'
-					break
-				case 4:
-					result = 'Machine Learning<br>(DS)'
-					break
-				case 5:
-					result = 'Lunch Time'
-					break
-				case 6:
-					result = 'Library'
-					break
-				case 7:
-					result = 'Project 2<br>(TP)'
-					break
-				case 8:
-					result = 'Project 2<br>(TP)'
-					break
-				case 9:
-					result = 'Project 2<br>(TP)'
-					break
-				default:
-					result = 'Something is Wrong'
-			}
-		} else if (weekday == 4) {
-			// Thursday
-			switch (period) {
-				case 1:
-					result = 'Neural Networks & Deep Learning <br>(VKS)'
-					break
-				case 2:
-					result = 'Project Management & Entrepreneurship<br>(RPC)'
-					break
-				case 3:
-					result = 'Multimedia Technology<br>(AD)'
-					break
-				case 4:
-					result = 'Machine Learning<br>(DS)'
-					break
-				case 5:
-					result = 'Lunch Time'
-					break
-				case 6:
-					result = 'Library'
-					break
-				case 7:
-					result = 'Project 2<br>(TP)'
-					break
-				case 8:
-					result = 'Project 2<br>(TP)'
-					break
-				case 9:
-					result = 'Project 2<br>(TP)'
-					break
-				default:
-					result = 'Something is Wrong'
-			}
-		} else if (weekday == 5) {
-			// Friday
-			switch (period) {
-				case 1:
-					result = 'Project 2<br>(TP)'
-					break
-				case 2:
-					result = 'Project 2<br>(TP)'
-					break
-				case 3:
-					result = 'Project 2<br>(TP)'
-					break
-				case 4:
-					result = 'Remedial Class'
-					break
-				case 5:
-					result = 'Lunch Time'
-					break
-				case 6:
-					result = 'Library'
-					break
-				case 7:
-					result = 'Project 2<br>(TP)'
-					break
-				case 8:
-					result = 'Project 2<br>(TP)'
-					break
-				case 9:
-					result = 'Project 2<br>(TP)'
-					break
-				default:
-					result = 'Something is Wrong'
-			}
-		} else if (weekday == 6) {
-			// Saturday
-			switch (period) {
-				case 1:
-					result = 'Project 2<br>(TP)'
-					break
-				case 2:
-					result = 'Project 2<br>(TP)'
-					break
-				case 3:
-					result = 'Project 2<br>(TP)'
-					break
-				case 4:
-					result = 'Remedial Class'
-					break
-				case 5:
-					result = 'Lunch Time'
-					break
-				case 6:
-					result = 'Library'
-					break
-				case 7:
-					result = 'Project 2<br>(TP)'
-					break
-				case 8:
-					result = 'Project 2<br>(TP)'
-					break
-				case 9:
-					result = 'Project 2<br>(TP)'
-					break
-				default:
-					result = 'Something is Wrong'
-			}
-		}
-		return result
-	}
-	getPeriodTime(period) {
-		let time = ''
-		switch (period) {
-			case 1:
-				time = '09:30 - 10:20'
-				break
-			case 2:
-				time = '10:20 - 11:10'
-				break
-			case 3:
-				time = '11:10 - 12:00'
-				break
-			case 4:
-				time = '12:00 - 12:50'
-				break
-			case 5:
-				time = '12:50 - 01:40'
-				break
-			case 6:
-				time = '01:40 - 02:30'
-				break
-			case 7:
-				time = '02:30 - 03:20'
-				break
-			case 8:
-				time = '03:20 - 04:10'
-				break
-			case 9:
-				time = '04:10 - 05:00'
-				break
-			default:
-				time = 'something went wrong!in time'
-		}
-		return time
-	}
+  getSubject(weekday, period) {
+    let result;
+    if (weekday == 2) {
+      // TuesDay
+      switch (period) {
+        case 1:
+          result = "E-Commerce and ERP<br>(KB)";
+          break;
+        case 2:
+          result = "T & P ACTV<br>(KB)";
+          break;
+        case 3:
+          result = "Cyber Law & Security<br>(PKP)";
+          break;
+        case 4:
+          result = "Cryptography & Network Security<br>(SC)";
+          break;
+        case 5:
+          result = "Lunch Time";
+          break;
+        case 6:
+          result = "-";
+          break;
+        case 7:
+          result = "-";
+          break;
+        case 8:
+          result = "-";
+          break;
+        case 9:
+          result = "-";
+          break;
+        default:
+          result = "Something is Wrong";
+      }
+    } else if (weekday == 3) {
+      // Wednesday
+      switch (period) {
+        case 1:
+          result = "Cyber Law & Security<br>(PKP)";
+          break;
+        case 2:
+          result = "E-Commerce and ERP<br>(KB)";
+          break;
+        case 3:
+          result = "Cryptography & Network Security<br>(SC)";
+          break;
+        case 4:
+          result = "T & P ACTV<br>(VKS)";
+          break;
+        case 5:
+          result = "Lunch Time";
+          break;
+        case 6:
+          result = "-";
+          break;
+        case 7:
+          result = "-";
+          break;
+        case 8:
+          result = "-";
+          break;
+        case 9:
+          result = "-";
+          break;
+        default:
+          result = "Something is Wrong";
+      }
+    } else if (weekday == 4) {
+      // Thursday
+      switch (period) {
+        case 1:
+          result = "T & P ACTV<br>(VKS+RD)";
+          break;
+        case 2:
+          result = "T & P ACTV<br>(KB)";
+          break;
+        case 3:
+          result = "Cryptography & Network Security<br>(SC)";
+          break;
+        case 4:
+          result = "Cyber Law & Security<br>(PKP)";
+          break;
+        case 5:
+          result = "Lunch Time";
+          break;
+        case 6:
+          result = "-";
+          break;
+        case 7:
+          result = "-";
+          break;
+        case 8:
+          result = "-";
+          break;
+        case 9:
+          result = "-";
+          break;
+        default:
+          result = "Something is Wrong";
+      }
+    } else if (weekday == 5) {
+      // Friday
+      switch (period) {
+        case 1:
+          result = "-";
+          break;
+        case 2:
+          result = "-";
+          break;
+        case 3:
+          result = "-";
+          break;
+        case 4:
+          result = "-";
+          break;
+        case 5:
+          result = "Lunch Time";
+          break;
+        case 6:
+          result = "-";
+          break;
+        case 7:
+          result = "-";
+          break;
+        case 8:
+          result = "-";
+          break;
+        case 9:
+          result = "-";
+          break;
+        default:
+          result = "Something is Wrong";
+      }
+    } else if (weekday == 6) {
+      // Saturday
+      switch (period) {
+        case 1:
+          result = "E-Commerce and ERP<br>(KB)";
+          break;
+        case 2:
+          result = "Cyber Law & Security<br>(PKP)";
+          break;
+        case 3:
+          result = "Cryptography & Network Security<br>(SC)";
+          break;
+        case 4:
+          result = "T & P ACTV<br>(TP)";
+          break;
+        case 5:
+          result = "Lunch Time";
+          break;
+        case 6:
+          result = "-";
+          break;
+        case 7:
+          result = "-";
+          break;
+        case 8:
+          result = "-";
+          break;
+        case 9:
+          result = "-";
+          break;
+        default:
+          result = "Something is Wrong";
+      }
+    }
+    return result;
+  }
+  getPeriodTime(period) {
+    let time = "";
+    switch (period) {
+      case 1:
+        time = "09:30 - 10:20";
+        break;
+      case 2:
+        time = "10:20 - 11:10";
+        break;
+      case 3:
+        time = "11:10 - 12:00";
+        break;
+      case 4:
+        time = "12:00 - 12:50";
+        break;
+      case 5:
+        time = "12:50 - 01:40";
+        break;
+      case 6:
+        time = "01:40 - 02:30";
+        break;
+      case 7:
+        time = "02:30 - 03:20";
+        break;
+      case 8:
+        time = "03:20 - 04:10";
+        break;
+      case 9:
+        time = "04:10 - 05:00";
+        break;
+      default:
+        time = "something went wrong!in time";
+    }
+    return time;
+  }
 }
--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,87 +1,104 @@
 //Create a day
-const day= new Day();
+const day = new Day();
 main();
 //Call the main function after 1 minute
-setInterval(main,60000);
+setInterval(main, 60000);
 //Main Function
-function main(){
-clearRoutine();
+function main() {
+  clearRoutine();
 
-//Get time and weekday 
-let today=new Date();
-let hours=today.getHours();
-let minute=today.getMinutes().toFixed(2);
-let time = hours+(minute/100);
-let weekday=today.getDay();
+  //Get time and weekday
+  let today = new Date();
+  let hours = today.getHours();
+  let minute = today.getMinutes().toFixed(2);
+  let time = hours + minute / 100;
+  let weekday = today.getDay();
 
-//Get ui's
-var tableBody=document.getElementById('table-body');
-var msg=document.querySelector('.msg');
+  //Get ui's
+  var tableBody = document.getElementById("table-body");
+  var msg = document.querySelector(".msg");
 
-//Add Heading
-if(time>=0.00 && time<=9.29){
-    let msg="Today's Class";
-    let externalday='Today';
-    showRoutine(weekday,msg,externalday);
-    
-} else if(time>=9.30 && time<=16.59){
-    let msg="Upcoming Class";
-    let externalday='Today';
-    showRoutine(weekday,msg,externalday);
-    if(weekday!=0 && weekday!==1){
-        if(time>=10.20){tableBody.firstElementChild.remove();}
-        if(time>=11.10){tableBody.firstElementChild.remove();}
-        if(time>=12.00){tableBody.firstElementChild.remove();}
-        if(time>=12.50){tableBody.firstElementChild.remove();}
-        if(time>=13.40){tableBody.firstElementChild.remove();}
-        if(time>=14.30){tableBody.firstElementChild.remove();}
-        if(time>=15.20){tableBody.firstElementChild.remove();}
-        if(time>=16.10){tableBody.firstElementChild.remove();}
+  //Add Heading
+  if (time >= 0.0 && time <= 9.29) {
+    let msg = "Today's Class";
+    let externalday = "Today";
+    showRoutine(weekday, msg, externalday);
+  } else if (time >= 9.3 && time <= 16.59) {
+    let msg = "Upcoming Class";
+    let externalday = "Today";
+    showRoutine(weekday, msg, externalday);
+    if (weekday != 0 && weekday !== 1) {
+      if (time >= 10.2) {
+        tableBody.firstElementChild.remove();
+      }
+      if (time >= 11.1) {
+        tableBody.firstElementChild.remove();
+      }
+      if (time >= 12.0) {
+        tableBody.firstElementChild.remove();
+      }
+      if (time >= 12.5) {
+        tableBody.firstElementChild.remove();
+      }
+      if (time >= 13.4) {
+        tableBody.firstElementChild.remove();
+      }
+      if (time >= 14.3) {
+        tableBody.firstElementChild.remove();
+      }
+      if (time >= 15.2) {
+        tableBody.firstElementChild.remove();
+      }
+      if (time >= 16.1) {
+        tableBody.firstElementChild.remove();
+      }
     }
-} else{
-    let msg="Tomorrow's Class";
+  } else {
+    let msg = "Tomorrow's Class";
     let nextweekday;
-    if(weekday==6){
-        nextweekday=0;
-    } else{
-        nextweekday=weekday+1;
+    if (weekday == 6) {
+      nextweekday = 0;
+    } else {
+      nextweekday = weekday + 1;
     }
-    let externalday='Tomorrow';
-    showRoutine(nextweekday,msg,externalday);
-}
+    let externalday = "Tomorrow";
+    showRoutine(nextweekday, msg, externalday);
+  }
 
-//check weekday is holiday or not
-function checkHoliday(weekday){
-    if(weekday==0||weekday==1){
-        return true;
+  //check weekday is holiday or not
+  function checkHoliday(weekday) {
+    if (weekday == 0 || weekday == 1) {
+      return true;
     }
     return false;
-}
+  }
 
-//show routine
-function showRoutine(weekday,msg,externalday){
-    document.querySelector('.heading').textContent=msg;
-    if(checkHoliday(weekday)){
-        let p=document.createElement('p');
-        p.className=" text-center display-4 alert alert-success";
-        p.appendChild(document.createTextNode(externalday+' is Holiday! Enjoy...'));
-        document.querySelector('.msg').appendChild(p);
-    } else{
-        for(let i=1;i<=9;i++){
-            let tr=document.createElement('tr');
-            let periodtd=tr.appendChild(document.createElement('td'));
-            periodtd.innerHTML=i;
-            let timetd=tr.appendChild(document.createElement('td'));
-            timetd.innerHTML=day.getPeriodTime(i);
-            let subjecttd=tr.appendChild(document.createElement('td'));
-            subjecttd.innerHTML=day.getSubject(weekday,i);
-            tableBody.appendChild(tr);
-        }
+  //show routine
+  function showRoutine(weekday, msg, externalday) {
+    document.querySelector(".heading").textContent = msg;
+    if (checkHoliday(weekday)) {
+      let p = document.createElement("p");
+      p.className = " text-center display-4 alert alert-success";
+      p.appendChild(
+        document.createTextNode(externalday + " is Holiday! Enjoy...")
+      );
+      document.querySelector(".msg").appendChild(p);
+    } else {
+      for (let i = 1; i <= 9; i++) {
+        let tr = document.createElement("tr");
+        let periodtd = tr.appendChild(document.createElement("td"));
+        periodtd.innerHTML = i;
+        let timetd = tr.appendChild(document.createElement("td"));
+        timetd.innerHTML = day.getPeriodTime(i);
+        let subjecttd = tr.appendChild(document.createElement("td"));
+        subjecttd.innerHTML = day.getSubject(weekday, i);
+        tableBody.appendChild(tr);
+      }
     }
-}
-//clear routine
-function clearRoutine(){
-    document.getElementById('table-body').innerHTML='';
-    document.querySelector('.msg').innerHTML='';
-}
+  }
+  //clear routine
+  function clearRoutine() {
+    document.getElementById("table-body").innerHTML = "";
+    document.querySelector(".msg").innerHTML = "";
+  }
 }
